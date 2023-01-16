1f512f2a6ea2d8023d70c09bb74066455b6c0aa2
HarmoNick777 <harmonick777@gmail.com>
2023-01-16 14:40:21 +0100

    Déplacement du fichier

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool isLaunched = false;
+    private float spawnDelay = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        StartCoroutine(SpawnDog());
+    }
+
+    IEnumerator SpawnDog()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isLaunched)
+        {
+            isLaunched = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            yield return new WaitForSeconds(spawnDelay);
+            isLaunched = false;
+        }
+    }
+}
